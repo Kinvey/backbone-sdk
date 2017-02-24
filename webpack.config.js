dc1bbe2eba42cc1478a948020e2a50f2fb7303eb
Thomas Conner <thomas@kinvey.com>
2017-02-24 16:29:21 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ var BANNER = '/**\n'
 
 module.exports = {
   context: path.resolve(__dirname, 'dist'),
-  entry: ['core-js/es6/symbol', 'es6-promise/auto', './index.js'],
+  entry: ['./index.js'],
   externals: {
     'backbone': 'umd Backbone'
   },
@@ -34,13 +34,8 @@ module.exports = {
   plugins: [
     new webpack.BannerPlugin(BANNER, { raw: true }),
     new webpack.NormalModuleReplacementPlugin(
-      /kinvey-node-sdk\/dist\/identity\/src\/popup\.js/,
+      /kinvey-js-sdk\/dist\/identity\/src\/popup\.js/,
       require.resolve(path.resolve(__dirname, 'node_modules/kinvey-phonegap-sdk/dist/popup.js'))
     )
-  ],
-  resolve: {
-    alias: {
-      request$: 'xhr'
-    }
-  }
+  ]
 };
